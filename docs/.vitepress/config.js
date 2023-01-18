ca50b40f6898d1021fa171125ec303252c61d952
kkigomi <kkigomi@duck.com>
2023-01-19 00:38:58 +0900

    라이센스 문구 추가

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -107,7 +107,10 @@ export default {
         docFooter: {
             prev: '이전',
             next: '다음'
-        }
+        },
+        footer: {
+            message: '<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="크리에이티브 커먼즈 라이선스" style="display: inline-block;border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />이 저작물은 <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">크리에이티브 커먼즈 저작자표시-동일조건변경허락 4.0 국제 라이선스</a>에 따라 이용할 수 있습니다.'
+        },
     },
     head: [
         [
